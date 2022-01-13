dbb270282aaa0efd267b24129d0ef595d112daac
angela tapia <tavata.canales@gmail.com>
2022-01-12 20:34:30 -0500

    se agrego el expres validator

--- controllers/Categoria.js
+++ controllers/Categoria.js
@@ -3,15 +3,27 @@ const Categoria = require("../models/Categoria")
 
 const crearCategoria = async (req = request, res = response) => {
     const { usuario, estado, ...newBody } = req.body
-    newBody.usuario=req.uid
-    const CategoriaDB= new Categoria(newBody)
+    newBody.usuario = req.uid
+    const CategoriaDB = new Categoria(newBody)
     await CategoriaDB.save()
     return res.json({
-        resutls:CategoriaDB,
-        ok:true,
-        msg:`Se ah creado la categoria`
+        resutls: CategoriaDB,
+        ok: true,
+        msg: `Se ah creado la categoria`
+    })
+}
+const actualizarCategoria = async (req = request, res = response) => {
+    const { id } = req.params
+    const { usuario, estado, ...newBody } = req.body
+    newBody.usuario = req.uid
+    const CategoriaDB = await Categoria.findByIdAndUpdate(id, newBody, { new: true })
+    return res.json({
+        results: CategoriaDB,
+        ok: true,
+        msg: `Categoria actualizada correctamente`
     })
 }
 module.exports = {
-    crearCategoria
+    crearCategoria,
+    actualizarCategoria
 }
--- routes/Categoria.js
+++ routes/Categoria.js
@@ -1,10 +1,15 @@
 const Router = require("express");
-const { crearCategoria } = require("../controllers/Categoria");
+const { check } = require("express-validator");
+const { crearCategoria, actualizarCategoria } = require("../controllers/Categoria");
 const { validarjwt } = require("../middlewares/validaciones");
+const { validarCampos } = require("../middlewares/validar-campos");
 const router = Router();
 router.post('/', [
     validarjwt
 ], crearCategoria)
-
-
+router.put('/:id', [
+    validarjwt,
+    check('nombre','el nombre es obligatorio').notEmpty(),
+    validarCampos
+], actualizarCategoria)
 module.exports = router
